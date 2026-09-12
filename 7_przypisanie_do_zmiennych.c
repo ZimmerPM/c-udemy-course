2c6d7dab44239d4af22b93b1597e2fbd88011af6
Piotr Zimirski <127020276+ZimmerPM@users.noreply.github.com>
2026-09-12 23:31:46 +0200

    Add input and output tasks

---
+++ 7_przypisanie_do_zmiennych.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    /*Tu umieść swoje rozwiązanie*/
+    
+    c = a - 2*b;
+
+    /*****************************/
+
+    printf("%d",c);
+
+    return 0;
+}
---
+++ 8_matematyka.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    float a, b, c, d;
+
+    scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &c);
+
+    d=2*a+b-c;
+
+    printf("%.1f", d);
+
+    return 0;
+}
