acb78a438c8d9625d4991ef8984ce51a62e97174
Piotr Zimirski <127020276+ZimmerPM@users.noreply.github.com>
2026-09-17 13:48:49 +0200

    Add compare operator programs

---
+++ 18_przypisanie_porownanie.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int x=10;
+    int y=12;
+
+    printf("x==y: %d\n", x==y);
+    printf("x=y: %d\n", x=y);
+    printf("x=%d\n", x);
+    
+    return 0;
+}
